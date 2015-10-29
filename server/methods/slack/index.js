8c5e553f10a31401b3922a5955d76c9c0298f7ca
Justin Rudio <justinjrudio@gmail.com>
2015-10-29 12:31:59 -0700

    Initialize slack methods on startup

--- server/methods/slack/index.js
+++ server/methods/slack/index.js
@@ -1,38 +1,34 @@
-Slack = {}
+Meteor.startup( () => {
+  Slack = {}
 
-let settings
+  let settings = Helpers.checkSettings('slack')
 
-try {
-  settings = Meteor.settings && Meteor.settings.slack
-} catch (e) {
-  return console.error(e.message)
-}
+  _.extend(Slack, {
+    tokens: {
+      media: settings.tokens.media,
+      m: settings.tokens.m,
+    },
+    hooks: {
+      incoming: settings.hooks.incoming
+    },
+    respondAsMediaBot (text, channel, cb) {
+      // console.log(text)
 
-_.extend(Slack, {
-  tokens: {
-    media: settings.tokens.media,
-    m: settings.tokens.m,
-  },
-  hooks: {
-    incoming: settings.hooks.incoming
-  },
-  respondAsMediaBot (text, channel, cb) {
-    // console.log(text)
+      let url = Slack.hooks.incoming
 
-    let url = Slack.hooks.incoming
+      let payload = {
+        channel,
+        text,
+        username: 'Media'
+      }
 
-    let payload = {
-      channel,
-      text,
-      username: 'Media'
+      payload = JSON.stringify(payload)
+
+      let params = {payload}
+
+      HTTP.post(url, {
+        params
+      }, cb)
     }
-
-    payload = JSON.stringify(payload)
-
-    let params = {payload}
-
-    HTTP.post(url, {
-      params
-    }, cb)
-  }
+  })
 })
