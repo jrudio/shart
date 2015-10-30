1c20de737b4a3e4286cfa1e624c5ab163b750b4b
Justin Rudio <justinjrudio@gmail.com>
2015-10-29 21:32:22 -0700

    parse command helpers

---
+++ server/commands/helpers/helpers.js
@@ -0,0 +1,39 @@
+CommandHelpers = {}
+
+_.extend(CommandHelpers, {
+  /**
+  * @param {String} Media requested
+  * returns {Promise}
+  */
+  parseShow (media) {
+    let result
+
+    let couchpotato = new Api.CouchPotato()
+
+    // console.log(couchpotato)
+
+    // Determine what to show:
+    // - individual media
+    // - wanted list
+    // - categories; recently released, in theaters, etc.
+    switch (media) {
+      case 'wanted':
+        result = couchpotato.wantedList
+        break
+      case 'charts':
+        result = couchpotato.charts
+        break
+      case 'test':
+        result = couchpotato.isAvailable
+          .then(r => r.success)
+          .then(r => CouchPotato.Format.test(r))
+        break
+      // Show individual media
+      default:
+        console.log('\"Show individual media\" is not implemented')
+        result = new Promise((resolve, reject) => reject('not-implemented'))
+    }
+
+    return result
+  }
+})
